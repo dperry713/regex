bb8575b97e3ecf85b120315c29310aa635873b38
dperry713 <dperry713@gmail.com>
2024-09-23 02:32:51 -0700

    commit

---
+++ task1.py
@@ -0,0 +1,7 @@
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+# Updated regex pattern to exclude emails from 'exclude.com'
+pattern = r"\b[A-Za-z0-9._%+-]+@(?!exclude\.com)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+emails = re.findall(pattern, text)
+print(emails)
